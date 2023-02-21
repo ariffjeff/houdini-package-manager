bc7c20e7bdf1016fbd828bc38c523536069cb54f
Ariff Jeff <ariffjeff@icloud.com>
2023-02-21 01:50:13 -0500

    setup main

--- houdini_package_manager/main.py
+++ houdini_package_manager/main.py
@@ -4,14 +4,32 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget
 
 
-def main() -> None:
+def main(start: bool = True, headless: bool = False) -> QApplication:
+    """Start package manager app
+
+    Create and open the Houdini package manager window
+
+    Args:
+        start (bool): Start the app. Default is True.
+            If False, the app won't start but the QApplication object will still be created.
+        headless (bool): The app window will not show when the app starts if True. Default is False.
+
+    Returns:
+        QApplication: PySide6.QtWidgets object
+    """
     app = QApplication(sys.argv)
 
+    TITLE = "Houdini Package Manager"
     window = QWidget()
-    window.setWindowTitle("Houdini Package Manager")
-    window.show()
+    window.setWindowTitle(TITLE)
 
-    app.exec()
+    if not headless:
+        window.show()
+
+    if start:
+        app.exec()
+
+    return app
 
 
 if __name__ == "__main__":
