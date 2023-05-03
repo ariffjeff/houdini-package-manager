ee98f0f8995db72e0c3bec591bf0847faa6b7122
Ariff Jeff <ariffjeff@icloud.com>
2023-05-03 15:35:54 -0400

    refactor packages layout to widget

--- houdini_package_manager/widgets/main_window.py
+++ houdini_package_manager/widgets/main_window.py
@@ -1,24 +1,23 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication,
-    QComboBox,
-    QHBoxLayout,
     QLabel,
     QMainWindow,
-    QPushButton,
-    QStackedWidget,
     QStatusBar,
     QTabWidget,
     QVBoxLayout,
     QWidget,
 )
 
-from houdini_package_manager.styles.widget_styles import WidgetStyles
-from houdini_package_manager.widgets.package_table import PackageTableModel
+from houdini_package_manager.widgets.window_packages import PackagesWidget
 from houdini_package_manager.wrangle.config_control import HoudiniManager
 
 
 class MainWindow(QMainWindow):
+    """
+    The main window containing all of HPM's functionality.
+    """
+
     def __init__(self, app: QApplication) -> None:
         super().__init__()
         self.app = app  # declare an app member
@@ -54,34 +53,7 @@ class MainWindow(QMainWindow):
         tabs.addTab(tab_packages, "Packages")
         tabs.addTab(tab_add_packages, "Add Local Packages")
 
-        # LABEL - HOUDINI VERSION DROPDOWN
-        label_version_dropdown = QLabel("HOUDINI VERSIONS")
-
-        # BUTTON - ADD PACKAGE
-        button_add_package = QPushButton()
-
-        WidgetStyles.svg_QPushButton(
-            button_add_package,
-            120,
-            36,
-            "./houdini_package_manager/icons/add_packages.svg",
-            "./houdini_package_manager/icons/add_packages_hover.svg",
-        )
-
-        # DROPDOWN - HOUDINI VERSION
-        combo_version = QComboBox()
-        combo_version.addItems(combo_version_labels)
-        combo_version.activated.connect(self.switch_package_table)
-
-        # BUTTONS - PACKAGE OPTIONS
-        button_copy = QPushButton("COPY")  # copy all the packages in the current table to another houdini version
-
-        # TABLE - PACKAGE DATA
-        table = PackageTableModel(self, self.houdini_data.hou_installs[self.versions[0]])
-        self.stacked_widget = QStackedWidget()
-        self.stacked_widget.addWidget(table)
-        # keep track of loaded package tables in the order they are added to stacked_widget
-        self.loaded_table_widgets = [self.versions[0]]
+        packages = PackagesWidget(self.houdini_data, self.versions, combo_version_labels)
 
         # CREATE LAYOUTS
         central_widget = QWidget(self)
@@ -90,45 +62,10 @@ class MainWindow(QMainWindow):
         layout_main_vertical = QVBoxLayout()  # includes the tabs
         central_widget.setLayout(layout_main_vertical)
 
-        layout_packages_vertical = QVBoxLayout()
-        layout_secondary_header = QHBoxLayout()
-        layout_package_options = QHBoxLayout()
-
         # SET LAYOUTS
         layout_main_vertical.addWidget(tabs)
 
-        tab_packages.setLayout(layout_packages_vertical)
-
-        layout_packages_vertical.addLayout(layout_secondary_header)
-        layout_packages_vertical.addLayout(layout_package_options)
-        layout_packages_vertical.addWidget(self.stacked_widget)
-
-        layout_secondary_header.addWidget(label_version_dropdown)
-        layout_secondary_header.addWidget(button_add_package)
-        layout_secondary_header.setAlignment(label_version_dropdown, Qt.AlignBottom)
-
-        layout_package_options.addWidget(combo_version)
-        layout_package_options.addWidget(button_copy)
+        tab_packages.setLayout(packages.layout_main)
 
     def quit_app(self):
         self.app.quit()
-
-    def switch_package_table(self):
-        """
-        Load a set of package data into a table, add it to the QStackedWidget, and switch to it.
-        If the set of data was already loaded then switch to it instead of loading it again.
-        """
-
-        combo_box = self.sender()
-        selected_item_text = combo_box.currentText().split(" ")[-1]
-
-        # if the table widget has already been added, switch to it
-        if selected_item_text in self.loaded_table_widgets:
-            index = self.loaded_table_widgets.index(selected_item_text)
-            self.stacked_widget.setCurrentIndex(index)
-            return
-
-        table = PackageTableModel(self, self.houdini_data.hou_installs[selected_item_text])
-        self.stacked_widget.addWidget(table)
-        self.stacked_widget.setCurrentWidget(table)
-        self.loaded_table_widgets.append(selected_item_text)
---
+++ houdini_package_manager/widgets/window_packages.py
@@ -0,0 +1,104 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (
+    QComboBox,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
+)
+
+from houdini_package_manager.styles.widget_styles import WidgetStyles
+from houdini_package_manager.widgets.package_table import PackageTableModel
+from houdini_package_manager.wrangle.config_control import HoudiniManager
+
+
+class PackagesWidget(QWidget):
+    """
+    The packages layout that displays the main packages table and relevant dropdowns and buttons.
+
+    Arguments:
+        table_data (HoudiniManager):
+            The data set to populate the QWidgetTable. Data can consist of multiple sets of packages
+            for different installed versions of Houdini.
+
+        versions (list[str]):
+            The list of ordered version numbers that will determine which set of package data is shown in the table.
+
+        header_labels (list[str]):
+            A list of ordered names for the column headers of the table. Determines how the columns are ordered.
+    """
+
+    def __init__(self, table_data: HoudiniManager, versions: list[str], header_labels: list[str]) -> None:
+        super().__init__()
+
+        self.table_data = table_data
+        self.versions = versions
+        self.header_labels = header_labels
+
+        # LABEL - HOUDINI VERSION DROPDOWN
+        label_version_dropdown = QLabel("HOUDINI VERSIONS")
+
+        # BUTTON - ADD PACKAGE
+        button_add_package = QPushButton()
+
+        WidgetStyles.svg_QPushButton(
+            button_add_package,
+            120,
+            36,
+            "./houdini_package_manager/icons/add_packages.svg",
+            "./houdini_package_manager/icons/add_packages_hover.svg",
+        )
+
+        # DROPDOWN - HOUDINI VERSION
+        combo_version = QComboBox()
+        combo_version.addItems(self.header_labels)
+        combo_version.activated.connect(self.switch_package_table)
+
+        # BUTTONS - PACKAGE OPTIONS
+        button_copy = QPushButton("COPY")  # copy all the packages in the current table to another houdini version
+
+        # TABLE - PACKAGE DATA
+        table = PackageTableModel(self, self.table_data.hou_installs[self.versions[0]])
+        self.stacked_widget = QStackedWidget()
+        self.stacked_widget.addWidget(table)
+        # keep track of loaded package tables in the order they are added to stacked_widget
+        self.loaded_table_widgets = [self.versions[0]]
+
+        # CREATE LAYOUTS
+        self.layout_main = QVBoxLayout()
+        layout_secondary_header = QHBoxLayout()
+        layout_package_options = QHBoxLayout()
+
+        # SET LAYOUTS
+        self.layout_main.addLayout(layout_secondary_header)
+        self.layout_main.addLayout(layout_package_options)
+        self.layout_main.addWidget(self.stacked_widget)
+
+        layout_secondary_header.addWidget(label_version_dropdown)
+        layout_secondary_header.addWidget(button_add_package)
+        layout_secondary_header.setAlignment(label_version_dropdown, Qt.AlignBottom)
+
+        layout_package_options.addWidget(combo_version)
+        layout_package_options.addWidget(button_copy)
+
+    def switch_package_table(self):
+        """
+        Load a set of package data into a table, add it to the QStackedWidget, and switch to it.
+        If the set of data was already loaded then switch to it instead of loading it again.
+        """
+
+        combo_box = self.sender()
+        selected_item_text = combo_box.currentText().split(" ")[-1]
+
+        # if the table widget has already been added, switch to it
+        if selected_item_text in self.loaded_table_widgets:
+            index = self.loaded_table_widgets.index(selected_item_text)
+            self.stacked_widget.setCurrentIndex(index)
+            return
+
+        table = PackageTableModel(self, self.table_data.hou_installs[selected_item_text])
+        self.stacked_widget.addWidget(table)
+        self.stacked_widget.setCurrentWidget(table)
+        self.loaded_table_widgets.append(selected_item_text)
