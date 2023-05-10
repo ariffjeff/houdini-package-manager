61caffe5c01148f91b05d8ab892c28335b1c404a
Ariff Jeff <ariffjeff@icloud.com>
2023-05-10 02:12:12 -0400

    full package creation functionality

--- houdini_package_manager/widgets/add_packages_layout.py
+++ houdini_package_manager/widgets/add_packages_layout.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 from PySide6.QtCore import QSize, Qt
@@ -19,8 +20,10 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
+from houdini_package_manager.meta.meta_tools import StatusBar, TextColor
 from houdini_package_manager.widgets.custom_widgets import SvgPushButton
 from houdini_package_manager.wrangle.config_control import HoudiniManager
+from houdini_package_manager.wrangle.package_templates import PackageTemplates
 
 
 class LocalPackageAdderWidget(QWidget):
@@ -46,28 +49,20 @@ class LocalPackageAdderWidget(QWidget):
         self.versions = versions
         self.tool_widgets = PresetWidgets()
 
-        # BUTTON - ADD PACKAGE
-        # button_add_package = SvgPushButton(
-        #     self,
-        #     120,
-        #     36,
-        #     "./houdini_package_manager/design/icons/add_packages.svg",
-        #     "./houdini_package_manager/design/icons/add_packages_hover.svg",
-        # )
-
         # LIST - HOUDINI VERSIONS
         label_versions = QLabel("Create packages for:")
         button_invert_versions_selection = self.tool_widgets.invert_selection(self)
         button_clear_versions_selection = self.tool_widgets.clear_selection(self)
 
-        layout_v_hou_versions = QVBoxLayout()
-        layout_v_hou_versions.setAlignment(Qt.AlignTop)
+        self.layout_v_hou_versions = QVBoxLayout()
+        self.layout_v_hou_versions.setAlignment(Qt.AlignTop)
         for version in self.versions:
             checkbox = QCheckBox(f"Houdini {version}")
             checkbox.setProperty("version", version)
-            layout_v_hou_versions.addWidget(checkbox)
-        button_invert_versions_selection.clicked.connect(lambda: self.invert_selection(layout_v_hou_versions))
-        button_clear_versions_selection.clicked.connect(lambda: self.clear_selection(layout_v_hou_versions))
+            checkbox.clicked.connect(self.update_file_overwrite_warning)
+            self.layout_v_hou_versions.addWidget(checkbox)
+        button_invert_versions_selection.clicked.connect(lambda: self.invert_selection(self.layout_v_hou_versions))
+        button_clear_versions_selection.clicked.connect(lambda: self.clear_selection(self.layout_v_hou_versions))
 
         group_versions = QGroupBox()
         group_versions.setStyleSheet(
@@ -78,7 +73,7 @@ class LocalPackageAdderWidget(QWidget):
             }
         """
         )
-        group_versions.setLayout(layout_v_hou_versions)
+        group_versions.setLayout(self.layout_v_hou_versions)
 
         # LIST - PLUGINS TO ADD
         label_plugins = QLabel("Plugins to add:")
@@ -88,8 +83,11 @@ class LocalPackageAdderWidget(QWidget):
         remove_selected_items = self.tool_widgets.remove_selected_items(self)
         button_remove_all_items = self.tool_widgets.remove_all_items(self)
 
-        plugin_list_box = PluginListBox(self)
-        plugin_list_box.setStyleSheet(
+        self.label_file_overwrite = QLabel("")
+        self.label_file_overwrite.setProperty("update", self.update_file_overwrite_warning)
+
+        self.plugin_list_box = PluginListBox(self, self.label_file_overwrite)
+        self.plugin_list_box.setStyleSheet(
             """
             PluginListBox {
                 border: 1px solid gray;
@@ -97,11 +95,25 @@ class LocalPackageAdderWidget(QWidget):
             }
         """
         )
-        button_add_items.clicked.connect(plugin_list_box.add_plugins_to_list)
-        button_invert_plugins_selection.clicked.connect(plugin_list_box.invert_selection)
-        button_clear_plugins_selection.clicked.connect(plugin_list_box.clear_selection)
-        remove_selected_items.clicked.connect(plugin_list_box.remove_selected_items)
-        button_remove_all_items.clicked.connect(plugin_list_box.remove_all_items)
+        button_add_items.clicked.connect(self.plugin_list_box.add_plugins_to_list)
+        button_invert_plugins_selection.clicked.connect(self.plugin_list_box.invert_selection)
+        button_clear_plugins_selection.clicked.connect(self.plugin_list_box.clear_selection)
+        remove_selected_items.clicked.connect(self.plugin_list_box.remove_selected_items)
+        button_remove_all_items.clicked.connect(self.plugin_list_box.remove_all_items)
+
+        # BUTTON - CREATE PACKAGES
+        button_add_package = SvgPushButton(
+            self,
+            120,
+            36,
+            "./houdini_package_manager/design/icons/add_packages_confirm.svg",
+            "./houdini_package_manager/design/icons/add_packages_confirm_hover.svg",
+        )
+        button_add_package.set_hover_status_message(
+            "Create packages for the selected plugins for the selected Houdini versions."
+        )
+        button_add_package.setToolTip("Create packages")
+        button_add_package.clicked.connect(self.create_packages)
 
         # CREATE LAYOUTS
         self.layout_main = QVBoxLayout()
@@ -117,7 +129,7 @@ class LocalPackageAdderWidget(QWidget):
         self.layout_main.addLayout(layout_h_versions_and_plugins)
         layout_h_versions_and_plugins.addLayout(layout_v_versions, 1)
         layout_h_versions_and_plugins.addLayout(layout_v_plugins, 4)
-        # self.layout_main.addWidget(button_add_package)
+        self.layout_main.addWidget(button_add_package)
 
         layout_v_versions.addLayout(layout_h_versions_header)
         layout_h_versions_header.addWidget(label_versions)
@@ -134,7 +146,110 @@ class LocalPackageAdderWidget(QWidget):
         layout_h_plugins_header.addWidget(remove_selected_items)
         layout_h_plugins_header.addWidget(button_remove_all_items)
         layout_h_plugins_header.setAlignment(label_plugins, Qt.AlignBottom)
-        layout_v_plugins.addWidget(plugin_list_box)
+        layout_v_plugins.addWidget(self.plugin_list_box)
+        layout_v_plugins.addWidget(self.label_file_overwrite)
+
+    def update_file_overwrite_warning(self) -> None:
+        """
+        The warning label will update to show the number of potential package config
+        overwrites, if any of the plugins' new config files may overwrite existing
+        config files in the selected Houdini versions' package directories.
+        """
+
+        versions_selected = self._selected_versions()
+        plugins_selected = self.plugin_list_box._selected_plugin_items()
+        if not versions_selected or not plugins_selected:
+            self.label_file_overwrite.setText("")
+            return
+
+        plugin_paths = [plugin.path for plugin in plugins_selected]
+        packages_directories = [
+            self.table_data.hou_installs[version].packages.packages_directory for version in versions_selected
+        ]
+
+        # assemble the paths of the new config files
+        expected_new_package_paths = []
+        for directory in packages_directories:
+            for path in plugin_paths:
+                expected_new_package_paths.append(Path(directory, path.name).with_suffix(".json"))
+
+        existing_paths = 0
+        for path in expected_new_package_paths:
+            if path.exists():
+                existing_paths += 1
+
+        if existing_paths == 0:
+            self.label_file_overwrite.setText("")
+            return
+
+        self.label_file_overwrite.setText(f"{existing_paths} existing config files will be overwritten.")
+
+    def _selected_versions(self) -> list[str]:
+        """
+        Return a list of the selected houdini version numbers from the version list.
+        """
+
+        widgets = []
+        for i in range(self.layout_v_hou_versions.count()):
+            widgets.append(self.layout_v_hou_versions.itemAt(i).widget())
+
+        widgets = [widget.property("version") for widget in widgets if widget.isChecked()]
+
+        return widgets
+
+    def _create_config_data(self, plugin_paths: list[Path]) -> dict:
+        """
+        Create the structured config data from a package template for each plugin path.
+        """
+
+        new_packages = {}
+        for path in plugin_paths:
+            new_packages[path.name] = PackageTemplates.standard(path)
+
+        return new_packages
+
+    def create_packages(self) -> None:
+        """
+        Create the package configs in the relevant Houdini packages
+        directories based on the selected Houdini versions and the selected
+        plugin items.
+        """
+
+        loaded = self.plugin_list_box._loaded_items()
+        if not loaded:
+            StatusBar.message("No plugins in list.", TextColor.ERROR)
+            return
+
+        plugins = self.plugin_list_box._selected_plugin_items()
+        if not plugins:
+            StatusBar.message("No plugins selected.", TextColor.ERROR)
+            return
+
+        versions = self._selected_versions()
+        if not versions:
+            StatusBar.message("No Houdini versions selected.", TextColor.ERROR)
+            return
+
+        plugin_paths = [plugin.path for plugin in plugins]
+        packages_directories = [
+            self.table_data.hou_installs[version].packages.packages_directory for version in versions
+        ]
+
+        new_packages = self._create_config_data(plugin_paths)
+
+        for directory in packages_directories:
+            for name, package in new_packages.items():
+                outpath = Path(directory, name).with_suffix(".json")
+                with open(outpath, "w") as outfile:
+                    json.dump(package, outfile, indent=4)
+
+        StatusBar.message(
+            f"Created {len(new_packages) * len(packages_directories)} total package configs for"
+            f" {len(packages_directories)} Houdini versions.",
+            TextColor.SUCCESS,
+        )
+
+        self.label_file_overwrite.property("update")()
 
     def clear_selection(self, layout) -> None:
         """
@@ -146,6 +261,8 @@ class LocalPackageAdderWidget(QWidget):
             if isinstance(box, QCheckBox):
                 box.setChecked(False)
 
+        self.label_file_overwrite.property("update")()
+
     def invert_selection(self, layout) -> None:
         """
         Invert the selection of houdini version list items.
@@ -156,11 +273,14 @@ class LocalPackageAdderWidget(QWidget):
             if isinstance(box, QCheckBox):
                 box.setChecked(not box.isChecked())
 
+        self.label_file_overwrite.property("update")()
+
 
 class PluginListBox(QStackedWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent, label_file_overwrite: QLabel):
         super().__init__(parent)
 
+        self.label_file_overwrite = label_file_overwrite
         # the system paths selected by the user from the dialog box
         self.selected_paths = []
 
@@ -202,20 +322,22 @@ class PluginListBox(QStackedWidget):
                 self.layout_plugin_list.removeItem(item)
 
         self.setCurrentIndex(0)
+        self.label_file_overwrite.property("update")()
 
     def remove_selected_items(self) -> None:
         """
         Remove only the selected items from the plugin list layout.
         """
 
-        checked_boxes = self._selected_items()
+        checked_boxes = self._selected_plugin_items()
         for widget in checked_boxes:
             self.layout_plugin_list.removeWidget(widget)
             widget.deleteLater()
 
-        # if any items left, change stack back to default
+        # if no items left, change stack back to default
         if self.layout_plugin_list.count() == 0:
             self.setCurrentIndex(0)
+        self.label_file_overwrite.property("update")()
 
     def invert_selection(self) -> None:
         """
@@ -225,6 +347,7 @@ class PluginListBox(QStackedWidget):
         items = self._loaded_items()
         for item in items:
             item.checkbox.toggle()
+        self.label_file_overwrite.property("update")()
 
     def clear_selection(self) -> None:
         """
@@ -234,8 +357,9 @@ class PluginListBox(QStackedWidget):
         items = self._loaded_items()
         for item in items:
             item.checkbox.setChecked(False)
+        self.label_file_overwrite.property("update")()
 
-    def _selected_items(self) -> list:
+    def _selected_plugin_items(self) -> list:
         """
         Return list of plugin item widgets that are selected (checked checkboxes).
         """
@@ -274,7 +398,7 @@ class PluginListBox(QStackedWidget):
         the user chooses the same path again from the dialog box.
         """
 
-        self.open_files_and_folders()
+        self._open_files_and_folders()
 
         if not self.selected_paths:
             return
@@ -283,12 +407,14 @@ class PluginListBox(QStackedWidget):
         loaded_paths = [widget.path for widget in self._loaded_items()]
         paths_to_add = [Path(path) for path in self.selected_paths if Path(path) not in loaded_paths]
         for path in paths_to_add:
-            self.layout_plugin_list.addWidget(PluginListItem(self, path))
+            self.layout_plugin_list.addWidget(PluginListItem(self, path, self.label_file_overwrite))
 
         self.setCurrentIndex(1)
         self.selected_paths = []
 
-    def open_files_and_folders(self) -> None:
+        self.label_file_overwrite.property("update")()
+
+    def _open_files_and_folders(self) -> None:
         """
         This is the absolute best attempt to get multiple file & folder selection working
         in one dialog box. It won't work using a native system dialog box, it only seems
@@ -330,7 +456,7 @@ class PluginListBox(QStackedWidget):
             self.selected_paths = dialog.selectedFiles()
         dialog.deleteLater()
 
-    def open_folder(self) -> None:
+    def _open_folder(self) -> None:
         # single folder selection
         folderpath = QFileDialog.getExistingDirectory(
             self,
@@ -347,11 +473,13 @@ class PluginListItem(QWidget):
     Includes a checkbox (with the path name), the path, and a remove-item button.
     """
 
-    def __init__(self, parent: QWidget, path: Path) -> None:
+    def __init__(self, parent: QWidget, path: Path, label_file_overwrite: QLabel) -> None:
         super().__init__(parent)
 
         if not isinstance(path, Path):
             raise TypeError("path must be a pathlib Path.")
+
+        self.label_file_overwrite = label_file_overwrite
 
         self.button_remove = SvgPushButton(
             self,
@@ -368,6 +496,7 @@ class PluginListItem(QWidget):
         self.name = path.name
         self.checkbox = QCheckBox(path.name)
         self.checkbox.setChecked(True)
+        self.checkbox.clicked.connect(self.toggled)
         self.label = QLabel(str(path))
         self.label.setStyleSheet("color: grey;")
 
@@ -378,12 +507,24 @@ class PluginListItem(QWidget):
         self.setLayout(layout_main)
         layout_main.setContentsMargins(0, 0, 0, 0)
 
+    def toggled(self):
+        """
+        Events that occur when the QCheckBox is clicked.
+        """
+
+        self.label_file_overwrite.property("update")()
+
     def remove_self(self):
         """
         Deletes this widget.
         """
 
         self.deleteLater()
+
+        # since the delete happens later and we need to update the file overwrite conflict check now,
+        # manually disable the toggle instead, which has the same effect in this case.
+        self.checkbox.setChecked(False)
+        self.label_file_overwrite.property("update")()
 
 
 class SelectPluginsButton(QPushButton):
