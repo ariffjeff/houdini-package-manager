6f7e6a1622a512c345fa0ed6b14086bdb18dde98
Ariff Jeff <ariffjeff@icloud.com>
2023-11-25 18:51:12 -0500

    only create log on crash
    in reality, it creates the log immediately on program start but deletes it at program end if no exception occurs. this is done because I can't figure out how to get the crash log to be created at crash time.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import os
 
 from houdini_package_manager.__init__ import __version__
 from houdini_package_manager.main import main
@@ -7,6 +8,7 @@ from houdini_package_manager.main import main
 if __name__ == "__main__":
     fmt = "%(levelname)s %(asctime)s - %(message)s"
 
+    # crash log file name currently stamped with program start time, not the crash time
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     FILENAME = f"Houdini_Package_Manager-{__version__}_crash.{current_time}.log"
 
@@ -17,7 +19,14 @@ if __name__ == "__main__":
     console_handler.setFormatter(logging.Formatter(fmt))
     logging.getLogger().addHandler(console_handler)
 
+    exception_occurred = False
+
     try:
         main()
     except Exception as e:
+        exception_occurred = True
         logging.exception("An internal error occured: %s", str(e))
+
+    if not exception_occurred:
+        logging.shutdown()  # Ensure all loggers are closed
+        os.remove(FILENAME)
