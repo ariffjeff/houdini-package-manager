0e5a1ae033cb492f0c55ae852bb38f0036706ab5
Ariff Jeff <ariffjeff@icloud.com>
2024-04-09 00:35:36 -0400

    fix template setting wrong key names
    they literally weren't even supported by houdini/mentioned in the documentation

--- houdini_package_manager/wrangle/package_templates.py
+++ houdini_package_manager/wrangle/package_templates.py
@@ -7,36 +7,35 @@ class PackageTemplates:
     """
 
     @staticmethod
-    def standard(plugin_path: Path):
+    def standard(houdini_path: Path):
         """
         A standard package template.
 
-        PLUGIN_PATH is a an arbitrary variable.
-
         Houdini will recognize:
-            recommends
-            path
+            HOUDINI_PATH
+            hpath (Shortcut to set HOUDINI_PATH. "path" has been deprecated in favor of hpath.)
             HOUDINI_TOOLBAR_PATH
             HOUDINI_OTLSCAN_PATH
             PYTHONPATH
+            enable
+            recommends
 
         Note: Do not include empty values for some keys, like recommends,
         because it was cause Houdini to crash on startup.
         """
 
-        if not plugin_path.exists():
-            raise FileNotFoundError(f"Does not exist: {plugin_path}")
+        if not houdini_path.exists():
+            raise FileNotFoundError(f"Does not exist: {houdini_path}")
 
-        plugin_path = str(plugin_path)
+        houdini_path = str(houdini_path)
 
         package = {
             "env": [
-                {"PLUGIN_PATH": plugin_path},
-                {"HOUDINI_OTLSCAN_PATH": "$PLUGIN_PATH/otls"},
-                {"HOUDINI_TOOLBAR_PATH": "$PLUGIN_PATH/toolbar"},
-                {"PYTHONPATH": "$PLUGIN_PATH/scripts/python"},
-            ],
-            "path": "$PLUGIN_PATH",
+                {"HOUDINI_PATH": houdini_path},
+                {"HOUDINI_OTLSCAN_PATH": "$HOUDINI_PATH/otls"},
+                {"HOUDINI_TOOLBAR_PATH": "$HOUDINI_PATH/toolbar"},
+                {"PYTHONPATH": "$HOUDINI_PATH/scripts/python"},
+            ]
         }
 
         return package
