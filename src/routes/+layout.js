bc9096556dcc7b28d2f446621d6bd9e47f2b2ad9
Ariff Jeff <ariffjeff@icloud.com>
2026-09-03 23:55:17 -0400

    set svelte to SSG for tauri

---
+++ src/routes/+layout.js
@@ -0,0 +1,4 @@
+// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
+// pages by setting this here, but should prerender as many as possible to
+// avoid large performance and SEO impacts
+export const prerender = true;
---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
